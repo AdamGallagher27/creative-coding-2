5cb1f03b539347b28148dbd0b0d31809371c30bf
Adam <N00211418@iadt.ie>
2023-04-25 09:23:22 +0100

    tidied code

--- Plane01/sketch.js
+++ Plane01/sketch.js
@@ -7,7 +7,6 @@ const apWidth = 50
 const apHeight = 70
 const apTail = 30
 const rotAngle = 90
-let planes = []
 
 let airField = new AirField
 
@@ -15,11 +14,6 @@ function setup() {
   createCanvas(screenWidth, screenHeight);
   background(200);
   angleMode(DEGREES);
-
-
-  for(let i = 0; i < 1; i++) {
-    planes.push(new Plane())
-  }
   
   airField.generatePlanes(4)
   console.log(airField)
@@ -29,26 +23,5 @@ function draw() {
   background(0);
 
   airField.flyPlanes()
-
-  // first plane
-  // push()
-  
-  // beginShape()
-  //   translate(250, 250)
-  //   rotate(rotAngle)
-  //   scale(apScale)
-  //   vertex(0, 0)
-  //   vertex(apWidth / 2, apTail)
-  //   vertex(0, -apHeight)
-  //   vertex(-(apWidth / 2), apTail)
-  //   vertex(0, 0)
-  // endShape()
-
-  // pop()
-
-  // planes.forEach(plane => {
-  //   plane.fly()
-  //   plane.render()
-  // });
 
 }
